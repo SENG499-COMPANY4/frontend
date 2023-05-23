3b316bdd7afbb89aad269fce617c30c7fad3d7e3
Mark Felea-Motet <markmotet@gmail.com>
2023-05-23 10:01:39 -0700

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "node_modules/preline/dist/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('preline/plugin'),
+  ],
+  variants: {
+    extend: {
+      borderColor: ['responsive', 'hover', 'focus', 'active'],
+      ringColor: ['responsive', 'hover', 'focus', 'active'],
+    },
+  },
+}
+
