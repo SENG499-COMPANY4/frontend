8e9d1a68171f684224f96b3f8bfec4ec9c64d9d2
Mark Felea-Motet <markfm@uvic.ca>
2023-05-26 15:27:36 -0700

    Log in button now pops up a template modal

--- src/components/ProfNavbar.js
+++ src/components/ProfNavbar.js
@@ -5,7 +5,8 @@ import { HiOutlineMail } from 'react-icons/hi'
 // NavBar component
 const ProfNavBar = () => {
 
-    return (
+  return (
+    <div>
       <header class="flex flex-wrap sm:justify-start sm:flex-nowrap z-50 w-full bg-white border-b border-gray-200 text-sm py-3 sm:py-0 dark:bg-gray-800 dark:border-gray-700">
         <nav class="relative max-w-7xl w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between sm:px-6 lg:px-8" aria-label="Global">
           <div class="flex items-center justify-between">
@@ -24,12 +25,12 @@ const ProfNavBar = () => {
           </div>
           <div id="navbar-collapse-with-animation" class="hs-collapse hidden overflow-hidden transition-all duration-300 basis-full grow sm:block">
             <div class="flex flex-col gap-y-4 gap-x-0 mt-5 sm:flex-row sm:items-center sm:justify-end sm:gap-y-0 sm:gap-x-7 sm:mt-0 sm:pl-7">
-              
+
               <a class="font-medium text-gray-500 hover:text-gray-400 sm:py-6 dark:text-gray-400 dark:hover:text-gray-500" href="/preferences">Set Preferences</a>
               <a class="font-medium text-gray-500 hover:text-gray-400 sm:py-6 dark:text-gray-400 dark:hover:text-gray-500" href="/schedule">View Schedule</a>
               {/* <a class="font-medium text-blue-600 hover:text-gray-400 sm:py-6 dark:text-blue-500 dark:hover:text-gray-500" href="#"><HiOutlineMail /></a> */}
-  
-              <a class="flex items-center gap-x-2 font-medium text-gray-500 hover:text-blue-600 sm:border-l sm:border-gray-300 sm:my-6 sm:pl-6 dark:border-gray-700 dark:text-gray-400 dark:hover:text-blue-500" href="#">
+
+              <a class="flex items-center gap-x-2 font-medium text-gray-500 hover:text-blue-600 sm:border-l sm:border-gray-300 sm:my-6 sm:pl-6 dark:border-gray-700 dark:text-gray-400 dark:hover:text-blue-500" href="#" data-hs-overlay="#login-modal">
                 <svg class="w-4 h-4" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
                   <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z" />
                 </svg>
@@ -39,12 +40,35 @@ const ProfNavBar = () => {
           </div>
         </nav>
       </header>
-  
-    );
-  }
+
+      <div id="login-modal" class="hs-overlay hidden w-full h-full fixed top-0 left-0 z-[60] overflow-x-hidden overflow-y-auto">
+        <div class="hs-overlay-open:mt-7 hs-overlay-open:opacity-100 hs-overlay-open:duration-500 mt-0 opacity-0 ease-out transition-all sm:max-w-lg sm:w-full m-3 sm:mx-auto min-h-[calc(100%-3.5rem)] flex items-center">
+          <div class="flex flex-col bg-white border shadow-sm rounded-xl dark:bg-gray-800 dark:border-gray-700 dark:shadow-slate-700/[.7]">
+
+            <div class="p-4 overflow-y-auto">
+              <p class="text-gray-800 dark:text-gray-400">
+                This is a wider card with supporting text below as a natural lead-in to additional content.
+              </p>
+            </div>
+            <div class="flex justify-end items-center gap-x-2 py-3 px-4 border-t dark:border-gray-700">
+              <button type="button" class="hs-dropdown-toggle py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border font-medium bg-white text-gray-700 shadow-sm align-middle hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-600 transition-all text-sm dark:bg-slate-900 dark:hover:bg-slate-800 dark:border-gray-700 dark:text-gray-400 dark:hover:text-white dark:focus:ring-offset-gray-800" data-hs-overlay="#login-modal">
+                Close
+              </button>
+              <a class="py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800" href="#">
+                Save changes
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+
+  );
+}
 
 
-                // <a class="font-medium" href="/preferences" aria-current="page">Set Preferences</a>
-                // <a class="font-medium" href="/professor" aria-current="page">View Schedule</a>
+// <a class="font-medium" href="/preferences" aria-current="page">Set Preferences</a>
+// <a class="font-medium" href="/professor" aria-current="page">View Schedule</a>
 
 export default ProfNavBar;
