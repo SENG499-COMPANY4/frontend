e7234c8181f39285b23b002b1c450d69183998ec
Vijaivir Dhaliwal <vijaivirdhaliwal@gmail.com>
2023-05-25 20:41:51 -0700

    added calendar

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Professor from './Professor';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
 
@@ -9,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Professor />
     </BrowserRouter>
   </React.StrictMode>
 );
