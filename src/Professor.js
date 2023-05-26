5dccb90744f76901aaf36eb2b5162b7194f67248
Vijaivir Dhaliwal <vijaivirdhaliwal@gmail.com>
2023-05-25 22:46:26 -0700

    added preferences page

--- src/Professor.js
+++ src/Professor.js
@@ -1,51 +1,20 @@
-import { Fragment } from 'react'
-import { Popover, Transition } from '@headlessui/react'
 import ProfNavbar from './components/ProfNavbar';
 import About from './pages/About';
 import { Routes, Route } from 'react-router-dom';
 import FullCalendar from '@fullcalendar/react' // must go before plugins
 import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
-import { ChevronDownIcon, PhoneIcon, PlayCircleIcon } from '@heroicons/react/20/solid'
-import {
-  ArrowPathIcon,
-  ChartPieIcon,
-  CursorArrowRaysIcon,
-  FingerPrintIcon,
-  SquaresPlusIcon,
-} from '@heroicons/react/24/outline'
-
-const solutions = [
-  { name: 'Analytics', description: 'Get a better understanding of your traffic', href: '#', icon: ChartPieIcon },
-  { name: 'Engagement', description: 'Speak directly to your customers', href: '#', icon: CursorArrowRaysIcon },
-  { name: 'Security', description: "Your customers' data will be safe and secure", href: '#', icon: FingerPrintIcon },
-  { name: 'Integrations', description: 'Connect with third-party tools', href: '#', icon: SquaresPlusIcon },
-  { name: 'Automations', description: 'Build strategic funnels that will convert', href: '#', icon: ArrowPathIcon },
-]
-const callsToAction = [
-  { name: 'Watch demo', href: '#', icon: PlayCircleIcon },
-  { name: 'Contact sales', href: '#', icon: PhoneIcon },
-]
 
 export default function Example() {
   return (
     <div class="container mx-auto px-10 max-w-screen-lg">
-      <ProfNavbar>
-
-      </ProfNavbar>
+      <ProfNavbar></ProfNavbar>
       <Routes>
         <Route path="/about" component={About} />
-        
       </Routes>
       <FullCalendar
         plugins={[ dayGridPlugin ]}
         initialView="dayGridMonth"
       />
-      
-
-
-
-
     </div>
-    
   )
 }
--- src/index.js
+++ src/index.js
@@ -3,19 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import Professor from './Professor';
+import ProfPreferences from './ProfPreferences';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Professor />
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/preferences" element={<ProfPreferences />} />
+        <Route path="/professor" element={<Professor />} />
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
