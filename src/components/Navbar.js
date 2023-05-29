aa873fd5e23fe7e101990becdc34b155814bbfc5
Mark Felea-Motet <markmotet@gmail.com>
2023-05-29 15:33:04 -0700

    Merged ProfNavBar and AdminNavBar into one Navbar with conditional rendering.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import logo from '../logo.png'
 import { HiOutlineMail } from 'react-icons/hi'
-import { BiHelpCircle } from 'react-icons/bi';
+import { BiHelpCircle, BiLogOut } from 'react-icons/bi';
 
 // NavBar component
-const AdminNavbar = () => {
+const Navbar = ({ isLoggedIn, isAdmin, isProfessor }) => {
   return (
     <div>
       <header class="flex flex-wrap sm:justify-start sm:flex-nowrap z-50 w-full bg-white border-b border-gray-200 text-sm py-3 sm:py-0 dark:bg-gray-800 dark:border-gray-700">
         <nav class="relative max-w-7xl w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between sm:px-6 lg:px-8" aria-label="Global">
           <div class="flex items-center justify-between">
-          <a href='/'>
+            <a href='/'>
               <img src={logo} class="flex-none h-auto w-24" alt="logo" />
             </a>
             {/* <img src={logo} className="App-logo" alt="logo" style={{ width: '150px', height: 'auto' }} /> */}
@@ -28,18 +28,56 @@ const AdminNavbar = () => {
           <div id="navbar-collapse-with-animation" class="hs-collapse hidden overflow-hidden transition-all duration-300 basis-full grow sm:block">
             <div class="flex flex-col gap-y-4 gap-x-0 mt-5 sm:flex-row sm:items-center sm:justify-end sm:gap-y-0 sm:gap-x-7 sm:mt-0 sm:pl-7">
 
-              <a class="font-medium text-gray-500 hover:text-gray-400 sm:py-6 dark:text-gray-400 dark:hover:text-gray-500" href="/admininbox"><HiOutlineMail /></a>
-              <a class="font-medium text-gray-500 hover:text-gray-400 sm:py-6 dark:text-gray-400 dark:hover:text-gray-500" href="/help"><BiHelpCircle /></a>
-              {/* <a class="font-medium text-blue-600 hover:text-gray-400 sm:py-6 dark:text-blue-500 dark:hover:text-gray-500" href="#"><HiOutlineMail /></a> */}
 
-              <a class="flex items-center gap-x-2 font-medium text-gray-500 hover:text-blue-600 sm:border-l sm:border-gray-300 sm:my-6 sm:pl-6 dark:border-gray-700 dark:text-gray-400 dark:hover:text-blue-500" href="#" data-hs-overlay="#login-modal">
-                <svg class="w-4 h-4" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-                  <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z" />
-                </svg>
-                Log in
-              </a>
+              {/* Admin inbox button */}
+              {isAdmin && isLoggedIn && (
+                <a class="font-medium text-gray-500 hover:text-gray-400 sm:py-6 dark:text-gray-400 dark:hover:text-gray-500" href="/admininbox"><HiOutlineMail /></a>
+              )}
+
+              {/* Admin help button */}
+              {isAdmin && isLoggedIn && (
+                <a class="font-medium text-gray-500 hover:text-gray-400 sm:py-6 dark:text-gray-400 dark:hover:text-gray-500" href="/help"><BiHelpCircle /></a>
+              )}
+
+              {/* Professor set preferences button */}
+              {isProfessor && isLoggedIn && (
+                <a class="font-medium text-gray-500 hover:text-gray-400 sm:py-6 dark:text-gray-400 dark:hover:text-gray-500" href="/preferences">Set Preferences</a>
+              )}
+
+              {/* Professor view schedule button */}
+              {isProfessor && isLoggedIn && (
+                <a class="font-medium text-gray-500 hover:text-gray-400 sm:py-6 dark:text-gray-400 dark:hover:text-gray-500" href="/schedule">View Schedule</a>
+              )}
 
 
+
+              {/* Log in button */}
+              {isLoggedIn ? (
+                // If logged in
+                <a class="flex items-center gap-x-2 font-medium text-gray-500 hover:text-blue-600 sm:border-l sm:border-gray-300 sm:my-3 sm:pl-6 dark:border-gray-700 dark:text-gray-400 dark:hover:text-blue-500">
+
+                  {/* Profile picture */}
+                  <div class="hs-dropdown relative inline-flex">
+                    <button id="hs-dropdown-unstyled" type="button" class="hs-dropdown-toggle inline-flex justify-center items-center gap-x-2">
+                      <img class="inline-block h-[2.375rem] w-[2.375rem] rounded-full ring-2 ring-white dark:ring-gray-80" src="https://images.unsplash.com/photo-1568602471122-7832951cc4c5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=300&h=300&q=80" alt="Image Description"></img>
+                    </button>
+                    <div class="hs-dropdown-menu transition-[opacity,margin] duration-[0.1ms] sm:duration-[150ms] hs-dropdown-open:opacity-100 opacity-0 sm:w-30 hidden z-10 bg-white sm:shadow-md rounded-lg p-2 dark:bg-gray-800 sm:dark:border dark:border-gray-700 dark:divide-gray-700 before:absolute top-full sm:border before:-top-5 before:left-0 before:w-full before:h-5">
+                      <a class="flex justify-between items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300" href="#">
+                        Log out <BiLogOut />
+                      </a>
+                    </div>
+                  </div>
+
+                </a>
+              ) : (
+                // If not logged in
+                <a class="flex items-center gap-x-2 font-medium text-gray-500 hover:text-blue-600 sm:border-l sm:border-gray-300 sm:my-6 sm:pl-6 dark:border-gray-700 dark:text-gray-400 dark:hover:text-blue-500" href="#" data-hs-overlay="#login-modal">
+                  <svg class="w-4 h-4" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                    <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z" />
+                  </svg>
+                  Log in
+                </a>
+              )}
 
             </div>
           </div>
@@ -49,7 +87,7 @@ const AdminNavbar = () => {
       <div id="login-modal" class="hs-overlay hidden w-full h-full fixed top-0 left-0 z-[60] overflow-x-hidden overflow-y-auto">
         <div class="hs-overlay-open:mt-7 hs-overlay-open:opacity-100 hs-overlay-open:duration-500 mt-0 opacity-0 ease-out transition-all sm:max-w-lg sm:w-full m-3 sm:mx-auto min-h-[calc(100%-3.5rem)] flex items-center">
           <div class="flex flex-col bg-white border shadow-sm rounded-xl dark:bg-gray-800 dark:border-gray-700 dark:shadow-slate-700/[.7]">
-            
+
             <div class="p-4 overflow-y-auto">
               <p class="text-gray-800 dark:text-gray-400">
                 This is a wider card with supporting text below as a natural lead-in to additional content.
@@ -74,4 +112,4 @@ const AdminNavbar = () => {
   );
 }
 
-export default AdminNavbar;
+export default Navbar;
--- src/index.js
+++ src/index.js
@@ -9,21 +9,29 @@ import Help from './pages/Help';
 import AdminInbox from './pages/AdminInbox';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from './components/Navbar';
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+
+    <Navbar isLoggedIn={true} isAdmin={true} isProfessor={false} />
+
     <body class="dark:bg-gray-800 dark:text-gray-500 min-h-screen ">
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/preferences" element={<ProfPreferences />} />
-        <Route path="/professor" element={<Professor />} />
-        <Route path="/administrator" element={<Administrator />} />
-        <Route path="/help" element={<Help />} />
-        <Route path="/admininbox" element={<AdminInbox />} />
-      </Routes>
-    </BrowserRouter>
+
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/preferences" element={<ProfPreferences />} />
+          <Route path="/professor" element={<Professor />} />
+          <Route path="/administrator" element={<Administrator />} />
+          <Route path="/help" element={<Help />} />
+          <Route path="/admininbox" element={<AdminInbox />} />
+        </Routes>
+      </BrowserRouter>
     </body>
   </React.StrictMode>
 );
