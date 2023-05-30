4e5e63ccf3fdbbe32b5cc503c298b01d8b25fdf5
Isabella Rojas <isabellarojas@MacBook-Air.hitronhub.home>
2023-05-30 16:39:03 -0700

    test

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -100,11 +100,11 @@ const Navbar = ({ isLoggedIn, isAdmin, isProfessor }) => {
               <a class="py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800" href="#">
                 Save changes
               </a>
->>>>>>> main:src/components/Navbar.js
             </div>
           </div>
         </div>
       </div>
+    </div>
 
   );
 }
