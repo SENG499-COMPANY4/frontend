4fb95ec9a7ba8545018d8e62b4f9ba0accb678dd
Mark Felea-Motet <markmotet@gmail.com>
2023-06-09 17:29:36 -0700

    Added convertJson function that converts the sample_schedule.json into the right format for fullcalendar.io

--- src/components/AdminSchedule.js
+++ src/components/AdminSchedule.js
@@ -2,35 +2,65 @@ import React from 'react';
 import FullCalendar from '@fullcalendar/react' // must go before plugins
 import resourceTimelinePlugin from '@fullcalendar/resource-timeline' // a plugin!
 
+import jsonData from '../mock_data/sample_schedule.json';
+
+function convertJson(input) {
+    let output = [];
+
+    // iterate over each item in the schedule
+    for (let item of input.schedule) {
+        // create the new format for the schedule items
+        let scheduleItem = {
+            resourceId: item.room,
+            title: item.coursename,
+            start: item.start,
+            end: item.end
+        };
+
+        // add the new schedule item to the output
+        output.push(scheduleItem);
+
+        // create the new format for the room items
+        let roomItem = {
+            id: item.room,
+            building: item.room,
+            title: item.room
+        };
+
+        // check if this room has already been added to the output
+        if (!output.some(outputItem => outputItem.id === roomItem.id)) {
+            // add the new room item to the output
+            output.push(roomItem);
+        }
+    }
+
+    return output;
+}
+
+  
+console.log(convertJson(jsonData));
+
 const AdminSchedule = () => {
     return (<div>
         <FullCalendar
             schedulerLicenseKey="CC-Attribution-NonCommercial-NoDerivatives"
             plugins={[resourceTimelinePlugin]}
+            editable={true}
             headerToolbar={{
                 left: 'today prev,next',
                 center: 'title',
                 right: 'resourceTimelineDay,resourceTimelineWeek'
             }}
-            editable={true}
             initialView='resourceTimelineDay'
             resourceGroupField='building'
             resources={[
-                { id: 'a', building: '460 Bryant', title: 'Auditorium A' },
-                { id: 'b', building: '460 Bryant', title: 'Auditorium B' },
-                { id: 'c', building: '460 Bryant', title: 'Auditorium C' },
-                { id: 'd', building: '460 Bryant', title: 'Auditorium D' },
-                { id: 'e', building: '460 Bryant', title: 'Auditorium E' },
+                { id: 'ECS123', building: 'Engineering & Computer Science', title: 'ECS 123' },
+                { id: 'ECS125', building: 'Engineering & Computer Science', title: 'ECS 125' },
+                { id: 'DTBA102', building: 'David Turpin', title: 'ECS 125' },
             ]}
             // slotDuration={'00:10'}
-
-            events={[
-                { resourceId: 'a', title: 'Event 1', start: '2023-06-09T10:00:00', end: '2023-06-09T10:00:00' },
-                { resourceId: 'b', title: 'Event 2', start: '2023-06-09T13:00:00', end: '2023-06-09T15:00:00' },
-                { resourceId: 'c', title: 'Event 3', start: '2023-06-10T10:00:00', end: '2023-06-10T12:00:00' },
-                { resourceId: 'd', title: 'Event 4', start: '2023-06-11T10:00:00', end: '2023-06-11T12:00:00' },
-                { resourceId: 'e', title: 'Event 5', start: '2023-06-12T10:00:00', end: '2023-06-12T12:00:00' },
-            ]}
+            contentHeight={'auto'}
+            events={convertJson(jsonData)}
         />
     </div>
     );
