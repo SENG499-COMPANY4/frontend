b215072665c0ebc166c403f1507df4c09822e985
Mark Felea-Motet <markmotet@gmail.com>
2023-06-15 21:00:10 -0700

    Clicking events makes them red (simulate locking in place)

--- src/components/AdminSchedule.js
+++ src/components/AdminSchedule.js
@@ -76,10 +76,11 @@ const AdminSchedule = () => {
 
     const [tooltip, setTooltip] = useState(null);
     const [tooltipContent, setTooltipContent] = useState('');
+    const [clickedEvents, setClickedEvents] = useState([]);
 
     const handleMouseEnter = (info) => {
-        // Change event color on hover
-        info.el.style.backgroundColor = '#2a67a4';
+        // // Change event color on hover
+        // info.el.style.backgroundColor = '#2a67a4';
 
         const content = `Course: ${info.event.title}\nProfessor: ${info.event.extendedProps.professor}\nBuilding: ${info.event.extendedProps.building}\nRoom: ${info.event.extendedProps.room}\nTime: ${convertTime(info.event.extendedProps.startTime)} - ${convertTime(info.event.extendedProps.endTime)}`;
 
@@ -97,15 +98,32 @@ const AdminSchedule = () => {
 
     const handleMouseLeave = () => {
 
-        // Change event color back to default
-        const events = document.getElementsByClassName('fc-event');
-        for (let i = 0; i < events.length; i++) {
-            events[i].style.backgroundColor = '#3788d8';
-        }
+        // // Change event color back to default
+        // const events = document.getElementsByClassName('fc-event');
+        // for (let i = 0; i < events.length; i++) {
+        //     events[i].style.backgroundColor = '#3788d8';
+        // }
 
         setTooltip(null);
         setTooltipContent('');
     }
+
+    const handleEventClick = (info) => {
+        const clickedId = info.event.id;
+        const indexOfClicked = clickedEvents.indexOf(clickedId);
+        const eventEl = info.el;
+      
+        if (indexOfClicked === -1) {
+          // Event was not clicked before, add it to clickedEvents and change its color to red
+          setClickedEvents([...clickedEvents, clickedId]);
+          eventEl.style.backgroundColor = 'red';
+        } else {
+          // Event was clicked before, remove it from clickedEvents and change its color back to default
+          setClickedEvents(clickedEvents.filter(id => id !== clickedId));
+          eventEl.style.backgroundColor = '#3788d8';
+        }
+      };
+
     return (
         <div>
             <FullCalendar
@@ -131,10 +149,9 @@ const AdminSchedule = () => {
                 contentHeight={'auto'}
                 events={events}
 
-
-
                 eventMouseEnter={handleMouseEnter}
                 eventMouseLeave={handleMouseLeave}
+                eventClick={handleEventClick}
             />
             {tooltip &&
                 <div className="absolute z-10 py-3 px-4 bg-white border text-sm text-gray-600 rounded-md shadow-md dark:bg-gray-900 dark:border-gray-700 dark:text-gray-400 whitespace-pre"
