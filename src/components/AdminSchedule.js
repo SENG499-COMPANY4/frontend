SENG499-COMPANY4/frontend

commit ba148bec5f6ab3c1925c9bd42cbbb4040615eed9

adding filter functionality

src/components/AdminSchedule.js
@@ -7,17 +7,24 @@ import interactionPlugin from "@fullcalendar/interaction";
 import API from "../api";
 
 const AdminSchedule = () => {
-
+    
+    const [filter, setFilter] = useState('');
+    const [isFilterVisible, setFilterVisible] = useState(false);
     const [tooltip, setTooltip] = useState(null);
     const [tooltipContent, setTooltipContent] = useState('');
     const [clickedEvents, setClickedEvents] = useState([]);
     const [ schedule, setSchedule] = useState([]);
     const [ resources, setResources] = useState([]);
     const [ events, setEvents] = useState([]);
+    const [professors, setProfessors] = useState([]);
+
 
     function convertJson() {
 
         let rooms = new Set();
+        let updatedResources = [];
+        let updatedEvents = [];
+        let extractedProfessors = [];
     
         // map day names to numbers
         const dayMap = {
@@ -42,7 +49,22 @@ const AdminSchedule = () => {
                     title: item.room
                 };
                 // add the new room item to the resources
-                resources.push(roomItem);
+
+                // Updated this code because i was getting errors when updating
+                // resrources with resource.push. So now I create new array and 
+                // update the state with the new array
+                updatedResources = Array.from(rooms).map(room => {
+                    return {
+                      id: room,
+                      building: room,
+                      title: room
+                    };
+                  });            
+            }
+
+            //Adding professors, to professor list for filter 
+            if (!extractedProfessors.includes(item.professor)) {
+                extractedProfessors.push(item.professor);
             }
     
             let startTime = item.start.split("T")[1];
@@ -65,11 +87,18 @@ const AdminSchedule = () => {
                     endTime: endTime
                 }
             };
+
+
             // add the new schedule item to the events
-            events.push(scheduleItem);
+            if ((scheduleItem.extendedProps.professor.toLowerCase() === filter.toLowerCase()) || filter === '' ){
+                updatedEvents.push(scheduleItem);
+            }
+
         }
-        setResources({resources});
-        setEvents({events});
+
+        setProfessors(Array.from(extractedProfessors));
+        setResources(updatedResources);
+        setEvents(updatedEvents);
         return;
     }
 
@@ -90,7 +119,7 @@ const AdminSchedule = () => {
         if(schedule.length > 0){
             convertJson(schedule);
         }
-    }, [schedule]);
+    }, [schedule,filter]);
 
     const convertTime = (time24) => {
         let [hours, minutes] = time24.split(':');
@@ -147,6 +176,25 @@ const AdminSchedule = () => {
 
     return (
         <div>
+            <div className="flex justify-end mb-4">
+                <div className="py-1 px-2 bg-white border text-sm text-gray-600 rounded-md shadow-md dark:bg-gray-900 dark:border-gray-700 dark:text-gray-400 whitespace-pre">
+                <select
+                    value={filter}
+                    onChange={(e) => setFilter(e.target.value)}
+                    className="fc-resourceTimelineDay-button fc-button fc-button-primary fc-button-active"
+                    title="day view"
+                    aria-pressed="true"
+                    >
+                    <option value="">All Professors</option>
+                    {professors.map((professor) => (
+                        <option key={professor} value={professor}>
+                        {professor}
+                        </option>
+                    ))}
+                    </select>
+                </div>
+            </div>
+
             <FullCalendar
                 schedulerLicenseKey="CC-Attribution-NonCommercial-NoDerivatives"
                 plugins={[resourceTimelinePlugin, interactionPlugin]}

commit cb65290410f1347fa0118ba875d0a693d322c3c5

Fixed bug introduced with ba148be that removed buildings from the schedule

src/components/AdminSchedule.js
@@ -22,6 +22,7 @@ const AdminSchedule = () => {
     function convertJson() {
 
         let rooms = new Set();
+        let roomMap = new Map();
         let updatedResources = [];
         let updatedEvents = [];
         let extractedProfessors = [];
@@ -39,27 +40,22 @@ const AdminSchedule = () => {
     
         // iterate over each item in the schedule
         for (let item of schedule) {
-            // check if this room has already been added to the rooms set
-            if (!rooms.has(item.room)) {
-                rooms.add(item.room);
-                // create the new format for the room items
-                let roomItem = {
-                    id: item.room,
-                    building: item.building,
-                    title: item.room
-                };
+            // check if this room has already been added to the rooms map
+            if (!roomMap.has(item.room)) {
+                roomMap.set(item.room, item.building);
+                
                 // add the new room item to the resources
 
                 // Updated this code because i was getting errors when updating
                 // resrources with resource.push. So now I create new array and 
                 // update the state with the new array
-                updatedResources = Array.from(rooms).map(room => {
+                updatedResources = Array.from(roomMap.entries()).map(([room, building]) => {
                     return {
-                      id: room,
-                      building: room,
-                      title: room
+                        id: room,
+                        building: building,
+                        title: room
                     };
-                  });            
+                });            
             }
 
             //Adding professors, to professor list for filter 