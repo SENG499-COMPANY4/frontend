0b29f1282f07c2865cc656daad91b64cae97f3ac
vijaivir <91633223+vijaivir@users.noreply.github.com>
2023-06-30 14:13:43 -0700

    Update AdminInbox.js

--- src/pages/AdminInbox.js
+++ src/pages/AdminInbox.js
@@ -131,7 +131,7 @@ const AdminInbox = () => {
                                                             <div class="py-2 first:pt-0 last:pb-0">
                                                             <a class="flex items-center gap-x-3 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300"
                                                                     href="#" onClick={() => togglePopup(index)}>
-                                                                    View Preference
+                                                                    View Preferences
                                                                 </a>
                                                             </div>
                                                             <div class="py-2 first:pt-0 last:pb-0">
